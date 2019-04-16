16b1a740a912ef191a298e0efb49a9bc41c390a5
ScottDenton <scottyd_14@hotmail.com>
2019-04-15 17:51:00 -0700

    thumbnail images working

--- src/components/StreamCard.js
+++ src/components/StreamCard.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import ReactTwitchEmbedVideo from 'react-twitch-embed-video'
+
 
 
 
 class Stream extends React.Component {
 
 
-
-
   render () {
     const stream=this.props.stream
+    const url = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${stream.user_name}-220x150.jpg`
     return(
       <div>
           <div className="card" onClick={() => this.props.handleClickOnStream(stream)} >
-            <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_riotgames-400x225.jpg" className="card-img-top" alt="..." />
+            <img src={url} className="card-img-top" alt="..." />
             <div className="card-body">
-              <a >
+              <a href='#'>
               <p className="card-text">{stream.user_name}</p>
               </a>
               <p className="card-text">{stream.title}</p>
