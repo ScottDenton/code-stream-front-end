1dab03e1befa3ce98f472e4da0d02c338c61af07
ScottSlatton <scottslatton.tech@gmail.com>
2019-04-17 08:37:07 -0700

    Readability Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
-import '../Home.css';
-import Stream from './Stream'
-
+import React, { Component } from "react";
+import "../Home.css";
+import Stream from "./Stream";
 
 class Home extends Component {
-
   render() {
     return (
       <div className="Home">
--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,85 +1,87 @@
-import React from 'react'
-import StreamCard from './StreamCard'
-import Jumbotron from './Jumbotron'
-
+import React from "react";
+import StreamCard from "./StreamCard";
+import Jumbotron from "./Jumbotron";
 
 class Stream extends React.Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       streams: [],
-      jumbotronStream:{id: "33691327920", user_id: "107939114", user_name: "DutchsinseOfficial", game_id: "509670"}
-    }
+      jumbotronStream: {
+        id: "33691327920",
+        user_id: "107939114",
+        user_name: "DutchsinseOfficial",
+        game_id: "509670"
+      }
+    };
   }
 
-  componentDidMount(){
-    fetch('http://localhost:3000/api/v1/livestreams')
-    .then(resp => resp.json())
-    .then(streams => {
-      this.setState({
-        streams: streams.data
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/livestreams")
+      .then(resp => resp.json())
+      .then(streams => {
+        this.setState({
+          streams: streams.data
+        });
+        // seed db with users
+        // streams.data.map(stream =>{
+        //     const body={
+        //       user: {
+        //         username: stream.user_name,
+        //         user_id: stream.user_id,
+        //         password: 'temp'
+        //       }
+        //     }
+        //     fetch('http://localhost:3000/api/v1/users', {
+        //       method: "POST",
+        //       headers: {
+        //         "Accept": "application/json",
+        //         "Content-Type": "application/json"
+        //       },
+        //       body: JSON.stringify(body)
+        //     }).then(resp => resp.json())
+        //   })
       })
-      // seed db with users
-      // streams.data.map(stream =>{
-    //     const body={
-    //       user: {
-    //         username: stream.user_name,
-    //         user_id: stream.user_id,
-    //         password: 'temp'
-    //       }
-    //     }
-    //     fetch('http://localhost:3000/api/v1/users', {
-    //       method: "POST",
-    //       headers: {
-    //         "Accept": "application/json",
-    //         "Content-Type": "application/json"
-    //       },
-    //       body: JSON.stringify(body)
-    //     }).then(resp => resp.json())
-    //   })
-     })
-      .then(streams=>{
-         const streamArray = this.state.streams.slice(0, 1)
-        this.setJumbotron(streamArray[0])
-      })
-    }
+      .then(streams => {
+        const streamArray = this.state.streams.slice(0, 1);
+        this.setJumbotron(streamArray[0]);
+      });
+  }
 
-
-  setJumbotron =(stream) => {
+  setJumbotron = stream => {
     this.setState({
       jumbotronStream: stream
-    })
-  }
+    });
+  };
 
-  handleClickOnStream =(stream) => {
-      this.setJumbotron(stream);
-      document.getElementById("twitch-embed").children[0].src= `https://embed.twitch.tv/?channel=${stream.user_name}`
-
-  }
+  handleClickOnStream = stream => {
+    this.setJumbotron(stream);
+    document.getElementById(
+      "twitch-embed"
+    ).children[0].src = `https://embed.twitch.tv/?channel=${stream.user_name}`;
+  };
 
   renderStreams = () => {
-      return this.state.streams.map(stream => {
-          return  <StreamCard
+    return this.state.streams.map(stream => {
+      return (
+        <StreamCard
           stream={stream}
           key={stream.title}
-          handleClickOnStream={this.handleClickOnStream}/>
+          handleClickOnStream={this.handleClickOnStream}
+        />
+      );
+    });
+  };
 
-      })
-  }
-
-
-  render () {
-    return(
+  render() {
+    return (
       <div>
-      <Jumbotron stream={this.state.jumbotronStream}/>
-      <h6>Live Streams </h6>
-        <div className="stream_card_container">
-          {this.renderStreams()}
-        </div>
+        <Jumbotron stream={this.state.jumbotronStream} />
+        <h6>Live Streams </h6>
+        <div className="stream_card_container">{this.renderStreams()}</div>
       </div>
-
-    )
+    );
   }
 }
 
-export default Stream ;
+export default Stream;
